25fbbfdc3a418cb47f8a1fe67d5d43e2512827e0
Loz Calver <lozcalver@bigfork.co.uk>
2023-05-24 11:41:02 +0100

    Refactor validator to allow disabling validation on all fields within a CompositeField

--- src/Validators/RequiredFields.php
+++ src/Validators/RequiredFields.php
@@ -3,6 +3,7 @@
 namespace Bigfork\SilverstripeFormtacular\Validators;
 
 use Bigfork\SilverstripeFormtacular\Extensions\FormFieldExtension;
+use SilverStripe\Forms\CompositeField;
 use SilverStripe\Forms\FormField;
 use SilverStripe\Forms\RequiredFields as SilverstripeRequiredFields;
 
@@ -10,7 +11,9 @@ class RequiredFields extends SilverstripeRequiredFields
 {
     public function php($data): bool
     {
-        foreach ($this->required as $i => $fieldName) {
+        $required = [];
+        // Normalise list of required fields
+        foreach ($this->required as $key => $fieldName) {
             if (!$fieldName) {
                 continue;
             }
@@ -19,16 +22,36 @@ class RequiredFields extends SilverstripeRequiredFields
                 $fieldName = $fieldName->getName();
             }
 
-            /** @var FormField|FormFieldExtension $formField */
-            $formField = $this->form->Fields()->dataFieldByName($fieldName);
-            if (!$formField) {
-                continue;
+            $required[$fieldName] = $key;
+        }
+
+        /** @var FormField|FormFieldExtension $formField */
+        $this->form->Fields()->recursiveWalk(function (FormField $formField) use ($required) {
+            if (empty($required)) {
+                return;
             }
 
-            if (!$formField->getIsValidationApplicable($this->form)) {
-                unset($this->required[$i]);
+            $fieldName = $formField->getName();
+
+            // If we've encountered a required field directly, check if validation applies
+            if (array_key_exists($fieldName, $required) && !$formField->getIsValidationApplicable($this->form)) {
+                $key = $required[$fieldName];
+                unset($this->required[$key], $required[$fieldName]);
             }
-        }
+
+            // If we've encountered a CompositeField, check if it has validation disabled
+            if ($formField instanceof CompositeField && !$formField->getIsValidationApplicable($this->form)) {
+                // If validation doesn't apply to this CompositeField, we can disable it on any required fields within
+                /** @var FormField $nestedField */
+                foreach ($formField->getChildren()->dataFields() as $nestedField) {
+                    $fieldName = $nestedField->getName();
+                    if (array_key_exists($fieldName, $required)) {
+                        $key = $required[$fieldName];
+                        unset($this->required[$key], $required[$fieldName]);
+                    }
+                }
+            }
+        });
 
         return parent::php($data);
     }
