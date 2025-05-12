4508ddb9f61a717b86a8c8a86da472ad17024acc
Loz Calver <loz@bigfork.co.uk>
2025-05-12 11:29:10 +0100

    Fix hasCheckedFewerThan(1)

--- client/src/js/rules/evaluateHasCheckedFewerThan.js
+++ client/src/js/rules/evaluateHasCheckedFewerThan.js
@@ -3,5 +3,11 @@
  */
 export default (form, config) => {
     const value = window.formtacular_getFieldValue(form, config);
+
+    // Special case - value will be "null" if no options have been checked
+    if (value === null) {
+        return true;
+    }
+
     return Array.isArray(value) && value.length < config.arguments[0];
 }
